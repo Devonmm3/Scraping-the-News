8ca89e47bef37efcabeb2ad0084d0fae08ec9119
DevonMM <Devon.m.moran@gmail.com>
2019-03-14 19:26:52 -0400

    articleroutes

---
+++ controllers/articles-routes.js
@@ -0,0 +1,120 @@
+var express = require("express");
+var request = require("request");
+var cheerio = require("cheerio");
+var Article = require("../models/userComments.js");
+var router = express.Router();
+
+//homepage
+
+//scraping needed data from Betches sup/save to mongo
+router.get("/scrape", function(req, res) {
+    request("https://betches.com/the-sup/", function(error, reponse, html) {
+        var $ = cheerio.load(html);
+
+        $("div.archiveList > article").each(function(i, element) {
+            var result = {};
+            //get title&description of each article, save them within the result object.  result.title saves the <a> tag from betches
+            result.title = $(element).children("div.item-info").children("h2.title").html();
+            //result.description saves the text
+                    result.description = $(element).children("div.item-info").children("p.teaser").children("a").text();
+            
+            //new entry, article model
+
+            var entry = new Article(result);
+
+            entry.save(function(err, doc) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log(doc);
+                }
+            });
+        });
+        res.redirect("/");
+    });
+});
+
+router.get("/articles", function(req, res) {
+    Article.find({})
+    .exec(function(err, doc) {
+        if (err) {
+            console.log(error);
+        }
+        else {
+            res.json(doc);
+        }
+    });
+});
+
+router.post("/save/:id", function(req,res) {
+    //need article id update saved property to true, as it needs to be.
+
+    Article.findOneAndUpdate({ "_id": req.params.id }, {"saved": true})
+    //query goes
+    .exec(function(err, doc) {
+        //errors
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("doc: ", doc);
+        }
+    });
+});
+
+//articles
+
+//objectid used to get the article
+
+router.get("/articles/:id", function(req, res) {
+    Article.findOne({ "_id": req.params.id })
+    .populate("comments")
+    .exec(function(error, doc) {
+        //error
+        if (error) {
+            console.log(error);
+        }
+        else {
+            res.json(doc);
+        }
+    });
+});
+
+//new comment
+router.post("/comment/:id", function(req,res) {
+    var addComment = new Comment(req.body);
+    addComment.save(function(error, addComment) {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            Article.findOneAndUpdate({"_id": req.params.id}, { $push: { "comments": addComment._id }}, {new: true})
+            .exec(function(err,doc) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log("doc: ", doc);
+                    res.send(doc);
+                }
+            });
+        }
+    });
+});
+
+//remove
+router.post("/unsave/:id", function(req, res) {
+    Article.findOneAndUpdate({ "_id": req.params.id }, { "saved": false })
+    .exec(function(err, doc) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("Article has been Removed");
+        }
+    });
+    res.redirect("/saved");
+});
+
+module.exports = router;
