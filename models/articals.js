171f503b993f58e1d39d3b6c39875ef7d39fea37
DevonMM <Devon.m.moran@gmail.com>
2019-03-14 19:50:17 -0400

    articlesjs

---
+++ models/articals.js
@@ -0,0 +1,34 @@
+//require mongoose database
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+    //title
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    //description
+    description: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    //save articles
+    saved: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    //save array of comments
+    comments:[{
+        type: Schema.OjectId,
+        ref:"comment"
+    }]
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
